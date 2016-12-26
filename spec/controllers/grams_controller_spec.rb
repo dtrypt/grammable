c09bdf3b18455e4bd8b7a3cb3b566c55a778d660
Russell Kranz <russkranz@yahoo.com>
2016-12-26 03:20:17 +0000

    11: added FactoryGirl and refactored test controller specs

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -15,11 +15,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should show the new form successfully" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       get :new
@@ -36,11 +32,7 @@ RSpec.describe GramsController, type: :controller do
 
     it "should post the user's gram (creating gram in our database)
     and send to gram's show page" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       post :create, gram: {message: 'Hello!'}
@@ -52,11 +44,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should properly show validation errors" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       post :create, gram: {message: ''}
